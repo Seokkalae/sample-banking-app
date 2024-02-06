20a05213c40ee2f8f7af49c472b679abc41ae1d6
seokkalae <unknown@>
2024-02-06 14:51:02 +0300

    add methods in repos for BankAccountEntity and HistoryEntity

--- src/main/java/org/seokkalae/samplebankingapp/repository/BankingAccountRepository.java
+++ src/main/java/org/seokkalae/samplebankingapp/repository/BankingAccountRepository.java
@@ -4,8 +4,20 @@ import org.seokkalae.samplebankingapp.entity.BankAccountEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BankingAccountRepository extends CrudRepository<BankAccountEntity, UUID> {
+
+    Optional<BankAccountEntity>
+    findBankAccountEntitiesByAccount_FirstNameAndAccount_LastNameAndAccount_PatronymicAndPin(String firstName,
+                                                                                       String lastName,
+                                                                                       String patronymic,
+                                                                                             String pin);
+
+    Optional<BankAccountEntity> findBankAccountEntitiesByIdAndPin(UUID id, String pin);
+
+    List<BankAccountEntity> findAllByAccount_Id(UUID id);
 }
--- src/main/java/org/seokkalae/samplebankingapp/repository/HistoryRepository.java
+++ src/main/java/org/seokkalae/samplebankingapp/repository/HistoryRepository.java
@@ -1,11 +1,15 @@
 package org.seokkalae.samplebankingapp.repository;
 
 import org.seokkalae.samplebankingapp.entity.HistoryEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface HistoryRepository extends CrudRepository<HistoryEntity, UUID> {
+
+    List<HistoryEntity> findAllByBankAccount_IdOrderByOperationTimestampTZDesc (UUID bankAccountId);
 }
